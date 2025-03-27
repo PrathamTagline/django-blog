b734cc298ad8ce15e5d9597ebdb21517f1d64e5e
PrathamTagline <prathamp.tagline@gmail.com>
2025-03-27 17:29:05 +0530

    all blog page and blog post page is set

--- blogs/urls.py
+++ blogs/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('allposts', views.blogs, name='blogs'),
+    path('post/<slug:slug>', views.blog_detail, name='blog_detail'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,13 +1,19 @@
-from django.shortcuts import render
-from .models import Blog,Category
-# Create your views here.
+from django.shortcuts import get_object_or_404, render
+from .models import Blog, Category
 
 def blogs(request):
-    all_blogs = Blog.objects.all()
+    all_blogs = Blog.objects.select_related('category').all()
     all_categories = Category.objects.values('name')
 
     context = {
         'all_blogs': all_blogs,
-        'all_categories':all_categories
+        'all_categories': all_categories
     }
     return render(request, 'blogs/blog_posts.html', context)
+
+def blog_detail(request, slug):
+    blog = get_object_or_404(Blog,slug=slug)
+    return render(request, 'blogs/blog_post_detials.html', {'blog': blog})
+
+
+
