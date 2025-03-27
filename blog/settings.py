f59791c7efff02ea059a2fea2af2f92efbf734b0
PrathamTagline <prathamp.tagline@gmail.com>
2025-03-27 10:11:39 +0530

    set SMTP setting in setting.py file

--- blog/settings.py
+++ blog/settings.py
@@ -22,8 +22,17 @@ AUTH_USER_MODEL = 'users.User'
 env = environ.Env(DEBUG=(bool, False))  # Define default values
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))  # Ensure .env is read
 
+# media settings
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Email settings
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
